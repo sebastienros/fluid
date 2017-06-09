8c4d3c115f995b809fd06d4f88fb3eae6cde1db7
Sebastien Ros <sebastienros@gmail.com>
2017-06-08 23:09:00 -0700

    Adding benchmark for DotLiquid

--- Fluid.Benchmarks/FluidBenchmarks.cs
+++ Fluid.Benchmarks/FluidBenchmarks.cs
@@ -1,8 +1,11 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using DotLiquid;
 
 namespace Fluid.Benchmarks
 {
     [MemoryDiagnoser]
+    [ShortRunJob]
     public class FluidBenchmarks
     {
         private const string _source1 = @"
@@ -18,6 +21,8 @@ namespace Fluid.Benchmarks
 </ul>
 ";
 
+        private const string _source2 = @"{{ image }}";
+
         private const string _source3 = @"
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
@@ -29,18 +34,71 @@ Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolor
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 ";
+
+        private const string _source4 = @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, {{ image }} eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+
+
         [Benchmark]
-        public FluidTemplate ParseSample()
+        public FluidTemplate ParseSampleFluid()
         {
             FluidTemplate.TryParse(_source1, out var template, out var messages);
             return template;
         }
 
         [Benchmark]
-        public FluidTemplate ParseLoremIpsum()
+        public Template ParseSampleDotLiquid()
+        {
+            var template = Template.Parse(_source1);
+            return template;
+        }
+
+        [Benchmark]
+        public FluidTemplate ParseLoremIpsumFluid()
         {
             FluidTemplate.TryParse(_source3, out var template, out var messages);
             return template;
+        }
+
+        [Benchmark]
+        public Template ParseLoremIpsumDotLiquid()
+        {
+            var template = Template.Parse(_source3);
+            return template;
+        }
+
+        [Benchmark]
+        public string RenderSimpleOuputFluid()
+        {
+            FluidTemplate.TryParse(_source2, out var template, out var messages);
+            var context = new TemplateContext();
+            context.SetValue("image", "kitten.jpg");
+            return template.Render(context);
+        }
+
+        [Benchmark]
+        public string RenderSimpleOuputDotLiquid()
+        {
+            var template = Template.Parse(_source2);
+            template.Assigns.Add("image", "kitten.jpg");
+            return template.Render();
+        }
+
+
+        [Benchmark]
+        public string RenderLoremSimpleOuputFluid()
+        {
+            FluidTemplate.TryParse(_source4, out var template, out var messages);
+            var context = new TemplateContext();
+            context.SetValue("image", "kitten.jpg");
+            return template.Render(context);
+        }
+
+        [Benchmark]
+        public string RenderLoreSimpleOuputDotLiquid()
+        {
+            var template = Template.Parse(_source4);
+            template.Assigns.Add("image", "kitten.jpg");
+            return template.Render();
         }
     }
 }
