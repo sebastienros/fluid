70c2906918fde84c1a34062fd7f88cfd7cf4225c
Sebastien Ros <sebastienros@gmail.com>
2020-11-18 11:12:56 -0800

    More unit tests for number filters

--- Fluid.Tests/TemplateTests.cs
+++ Fluid.Tests/TemplateTests.cs
@@ -734,5 +734,17 @@ shape: '{{ shape }}'");
             var result = await template.RenderAsync(context);
             Assert.Equal("John", result);
         }
+
+        [Theory]
+        [InlineData("{{ '5' | plus: 5 }}", "10")]
+        [InlineData("{{ '5' | plus: 5.0 }}", "10.0")]
+        [InlineData("{{ '5' | times: 5 }}", "25")]
+        [InlineData("{{ '5' | minus: 5 }}", "0")]
+        [InlineData("{{ '21' | divided_by: 3 }}", "7")]
+        [InlineData("{{ '8888' | modulo: 10 }}", "8")]
+        public Task ShouldConvertNumber(string source, string expected)
+        {
+            return CheckAsync(source, expected);
+        }
     }
 }
