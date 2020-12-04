505354829e52cab3b85dae9e4d9c285ef65754a9
hishamco <hishamco_2007@yahoo.com>
2020-12-04 21:08:05 +0300

    Add color_to_rgb filter

---
+++ Fluid.Tests/ColorFiltersTests.cs
@@ -0,0 +1,30 @@
+﻿using Fluid.Values;
+using Fluid.Filters;
+using Xunit;
+
+namespace Fluid.Tests
+{
+    public class ColorFiltersTests
+    {
+        [Theory]
+        [InlineData("#ffffff", "rgb(255, 255, 255)")]
+        [InlineData("#fff", "rgb(255, 255, 255)")]
+        [InlineData("#000", "rgb(0, 0, 0)")]
+        [InlineData("#f00", "rgb(255, 0, 0)")]
+        [InlineData("#0f0", "rgb(0, 255, 0)")]
+        [InlineData("#00f", "rgb(0, 0, 255)")]
+        [InlineData("#7ab55c", "rgb(122, 181, 92)")]
+        public void ToRgb(string hexColor, string expected)
+        {
+            // Arrange
+            var input = new StringValue(hexColor);
+            var context = new TemplateContext();
+
+            // Act
+            var result = ColorFilters.ToRgb(input, FilterArguments.Empty, context);
+
+            // Assert
+            Assert.Equal(expected, result.ToStringValue());
+        }
+    }
+}
