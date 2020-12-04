57230c14eae7264abce6131f064cd7649c07c6bc
hishamco <hishamco_2007@yahoo.com>
2020-12-05 02:50:47 +0300

    Refactoring

--- Fluid/Filters/ColorFilters.cs
+++ Fluid/Filters/ColorFilters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Text.RegularExpressions;
 using Fluid.Values;
 
 namespace Fluid.Filters
@@ -17,63 +18,183 @@ namespace Fluid.Filters
 
         public static FluidValue ToRgb(FluidValue input, FilterArguments arguments, TemplateContext context)
         {
-            var htmlColor = input.ToStringValue();
-            var rgbColor = ColorTranslator.FromHtml(htmlColor);
-            if (rgbColor == Color.Empty)
+            var value = input.ToStringValue();
+            if (!HexColor.TryParse(value, out RgbColor color))
             {
                 return NilValue.Empty;
             }
 
-            return new StringValue($"rgb({rgbColor.R}, {rgbColor.G}, {rgbColor.B})");
+            return new StringValue(color.ToString());
         }
 
         public static FluidValue ToHex(FluidValue input, FilterArguments arguments, TemplateContext context)
         {
-            var color = input.ToStringValue();
-            var rgbColor = GetRgbColor(color);
-            var htmlColor = ColorTranslator.ToHtml(rgbColor);
-            if (htmlColor == String.Empty)
+            var value = input.ToStringValue();
+            if (!RgbColor.TryParse(value, out Color color))
             {
                 return NilValue.Empty;
             }
 
-            return new StringValue(htmlColor.ToLower());
+            var hexColor = new HexColor(color);
+
+            return new StringValue(hexColor.ToString());
         }
 
         public static FluidValue ToHsl(FluidValue input, FilterArguments arguments, TemplateContext context)
         {
-            var color = input.ToStringValue();
-            if (!(color.StartsWith("#") || color.StartsWith("rgb(")))
+            var value = input.ToStringValue();
+            if (RgbColor.TryParse(value, out Color color) == false && HexColor.TryParse(value, out color) == false)
             {
                 return NilValue.Empty;
             }
 
-            var rgbColor = color.StartsWith("#")
-                ? ColorTranslator.FromHtml(color)
-                : GetRgbColor(color);
-            var hslColor = HslColorTranslator.FromRgb(rgbColor);
+            var hslColor = HslColor.FromRgb(color);
 
-            return new StringValue($"({hslColor.H}, {Convert.ToInt32(hslColor.S * 100.0)}%, {Convert.ToInt32(hslColor.L * 100.0)}%)");
+            return new StringValue(hslColor.ToString());
         }
 
-        private static Color GetRgbColor(string rgbColorString)
+        private struct HexColor
         {
-            if (!(rgbColorString.StartsWith("rgb(") && rgbColorString.EndsWith(")")))
+            public HexColor(Color color)
             {
-                return Color.Empty;
+                R = color.R.ToString("X2", null);
+                G = color.G.ToString("X2", null);
+                B = color.B.ToString("X2", null);
             }
 
-            var rgbColor = rgbColorString.Substring(4, rgbColorString.Length - 5).Split(',');
-            if (rgbColor.Length != 3)
+            public string R { get; }
+
+            public string G { get; }
+
+            public string B { get; }
+
+            public static string FromRgb(Color color) => new HexColor(color).ToString();
+
+            public static Color ToRgb(string color) => RgbColor.FrmHex(color);
+
+            public static bool TryParse(string value, out RgbColor color)
             {
-                return Color.Empty;
+                if (Regex.IsMatch(value, "^#(?:[0-9a-fA-F]{3}){1,2}$"))
+                {
+                    var rgbColor = RgbColor.FrmHex(value);
+                    color = new RgbColor(rgbColor);
+
+                    return true;
+                }
+                else
+                {
+                    color = RgbColor.Empty;
+
+                    return false;
+                }
             }
 
-            var red = Convert.ToInt32(rgbColor[0]);
-            var green = Convert.ToInt32(rgbColor[1]);
-            var blue = Convert.ToInt32(rgbColor[2]);
+            public static bool TryParse(string value, out Color color)
+            {
+                if (Regex.IsMatch(value, "^#(?:[0-9a-fA-F]{3}){1,2}$"))
+                {
+                    color = RgbColor.FrmHex(value);
 
-            return Color.FromArgb(red, green, blue);
+                    return true;
+                }
+                else
+                {
+                    color = Color.Empty;
+
+                    return false;
+                }
+            }
+
+            public override string ToString() => $"#{R}{G}{B}".ToLower();
+        }
+
+        private struct RgbColor
+        {
+            public static readonly RgbColor Empty = default;
+
+            public RgbColor(Color color) : this(color.R, color.G, color.B)
+            {
+
+            }
+
+            public RgbColor(double red, double green, double blue)
+            {
+                R = red;
+                G = green;
+                B = blue;
+            }
+
+            public double R { get; }
+
+            public double G { get; }
+
+            public double B { get; }
+
+            public static bool TryParse(string value, out Color color)
+            {
+                if (Regex.IsMatch(value, @"^rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)$"))
+                {
+                    var rgbColor = value.Substring(4, value.Length - 5).Split(',');
+                    var red = Convert.ToInt32(rgbColor[0]);
+                    var green = Convert.ToInt32(rgbColor[1]);
+                    var blue = Convert.ToInt32(rgbColor[2]);
+
+                    color = Color.FromArgb(red, green, blue);
+
+                    return true;
+                }
+                else
+                {
+                    color = Color.Empty;
+
+                    return false;
+                }
+            }
+
+            public static Color FrmHex(string hexColor)
+            {
+                var color = Color.Empty;
+                if ((hexColor == null) || (hexColor.Length == 0))
+                {
+                    return color;
+                }
+
+                if ((hexColor[0] == '#') && (hexColor.Length == 7 || hexColor.Length == 4))
+                {
+                    if (hexColor.Length == 7)
+                    {
+                        color = Color.FromArgb(Convert.ToInt32(hexColor.Substring(1, 2), 16),
+                                           Convert.ToInt32(hexColor.Substring(3, 2), 16),
+                                           Convert.ToInt32(hexColor.Substring(5, 2), 16));
+                    }
+                    else
+                    {
+                        var r = Char.ToString(hexColor[1]);
+                        var g = Char.ToString(hexColor[2]);
+                        var b = Char.ToString(hexColor[3]);
+                        color = Color.FromArgb(Convert.ToInt32(r + r, 16),
+                                           Convert.ToInt32(g + g, 16),
+                                           Convert.ToInt32(b + b, 16));
+                    }
+                }
+
+                return color;
+            }
+
+            public static string ToHex(Color color)
+            {
+                var colorString = String.Empty;
+                if (color.IsEmpty)
+                {
+                    return colorString;
+                }
+
+                colorString = "#" + color.R.ToString("X2", null) + color.G.ToString("X2", null) + color.B.ToString("X2", null);
+
+                return colorString;
+            }
+
+            public override string ToString() => $"rgb({R}, {G}, {B})";
         }
 
         private struct HslColor
@@ -90,15 +211,12 @@ namespace Fluid.Filters
             public double S { get; }
 
             public double L { get; }
-        }
 
-        private static class HslColorTranslator
-        {
             public static HslColor FromRgb(Color color)
             {
-                var r = ((double)color.R / 255.0);
-                var g = ((double)color.G / 255.0);
-                var b = ((double)color.B / 255.0);
+                var r = color.R / 255.0;
+                var g = color.G / 255.0;
+                var b = color.B / 255.0;
                 var max = Math.Max(r, Math.Max(g, b));
                 var min = Math.Min(r, Math.Min(g, b));
                 var h = 0.0;
@@ -129,54 +247,9 @@ namespace Fluid.Filters
 
                 return new HslColor(h, s, l);
             }
+
+            public override string ToString()
+                => $"({H}, {Convert.ToInt32(S * 100.0)}%, {Convert.ToInt32(L * 100.0)}%)";
         }
-
-#if !NET461
-        private static class ColorTranslator
-        {
-            public static Color FromHtml(string htmlColor)
-            {
-                var color = Color.Empty;
-                if ((htmlColor == null) || (htmlColor.Length == 0))
-                {
-                    return color;
-                }
-
-                if ((htmlColor[0] == '#') && (htmlColor.Length == 7 || htmlColor.Length == 4))
-                {
-                    if (htmlColor.Length == 7)
-                    {
-                        color = Color.FromArgb(Convert.ToInt32(htmlColor.Substring(1, 2), 16),
-                                           Convert.ToInt32(htmlColor.Substring(3, 2), 16),
-                                           Convert.ToInt32(htmlColor.Substring(5, 2), 16));
-                    }
-                    else
-                    {
-                        var r = Char.ToString(htmlColor[1]);
-                        var g = Char.ToString(htmlColor[2]);
-                        var b = Char.ToString(htmlColor[3]);
-                        color = Color.FromArgb(Convert.ToInt32(r + r, 16),
-                                           Convert.ToInt32(g + g, 16),
-                                           Convert.ToInt32(b + b, 16));
-                    }
-                }
-
-                return color;
-            }
-
-            public static string ToHtml(Color color)
-            {
-                var colorString = String.Empty;
-                if (color.IsEmpty)
-                {
-                    return colorString;
-                }
-
-                colorString = "#" + color.R.ToString("X2", null) + color.G.ToString("X2", null) + color.B.ToString("X2", null);
-
-                return colorString;
-            }
-        }
-#endif
     }
 }
