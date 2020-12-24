0acaa6babb8f4eb3d0067fe1d3e3611e1f009bae
hishamco <hishamco_2007@yahoo.com>
2020-12-25 01:10:41 +0300

    Fix values

--- Fluid.Tests/ColorFiltersTests.cs
+++ Fluid.Tests/ColorFiltersTests.cs
@@ -271,8 +271,8 @@ namespace Fluid.Tests
 
         [Theory]
         [InlineData("#fff00f", new object[] { "#0b72ab" }, 129)]
-        [InlineData("rgb(255, 240, 15)", new object[] { "rgb(171, 205, 239)" }, 129)]
-        [InlineData("hsl(56, 100%, 53%)", new object[] { "hsl(210, 68%, 80%)" }, 129)]
+        [InlineData("rgb(255, 240, 15)", new object[] { "rgb(11, 114, 171)" }, 129)]
+        [InlineData("hsl(56, 100%, 53%)", new object[] { "hsl(201, 88%, 36%)" }, 129)]
         public void BrightnessDifference(string color, object[] arguments, decimal expected)
         {
             // Arrange
