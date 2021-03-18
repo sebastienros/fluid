b70930070f03fce30ce9b0b071c8c797b658dcb8
Soar360 <Soar360@live.com>
2021-03-18 16:21:31 +0800

    Add some unit tests.

---
+++ Fluid.Tests/Paginating/IdListPaginateValue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fluid.Paginating;
+using Fluid.Values;
+
+namespace Fluid.Tests.Paginating
+{
+    public class IdListPaginateValue : PaginationValue
+    {
+        private readonly List<int> _dataList;
+
+        public IdListPaginateValue()
+        {
+            _dataList = Enumerable.Range(1, 100).ToList();
+        }
+
+        protected override string GetUrl(int page)
+        {
+            return $"index.aspx?page={page}";
+        }
+
+        protected override Task<QueryResult> QueryAsync(int page, int pageSize)
+        {
+            var list = _dataList
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(i => NumberValue.Create(i));
+            var result = new QueryResult
+            {
+                ItemCount = _dataList.Count
+            };
+            result.AddRange(list);
+            return Task.FromResult(result);
+        }
+    }
+}
--- Fluid/Paginating/PaginationFilters.cs
+++ Fluid/Paginating/PaginationFilters.cs
@@ -1,35 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Fluid.Values;
 
 namespace Fluid.Paginating
 {
     public static class PaginationFilters
     {
+        private const string DefaultPaginationTemplate =
+            @"{%- if paginate.__Previous__ and paginate.__Previous__.__IsLink__ -%}
+<span class=""prev""><a href=""{{paginate.__Previous__.__Url__}}"">{{ txt_previous | default: paginate.__Previous__.__Title__ }}</a></span>
+{%-endif-%}
+{%- for pair in paginate.__Parts__ -%}
+{%- if pair.__IsLink__ -%}
+<span class=""page""><a href=""{{pair.__Url__}}"">{{pair.__Title__}}</a></span>
+{%- else -%}
+<span class=""deco"">{{pair.__Title__}}</span>
+{%- endif -%}
+{%- endfor -%}
+{%- if paginate.__Next__ and paginate.__Next__.__IsLink__ -%}
+<span class=""prev""><a href=""{{paginate.__Next__.__Url__}}"">{{ txt_next | default: paginate.__Next__.__Title__ }}</a></span>
+{%- endif -%}";
+
+        private static readonly FluidParser Parser = new();
+
+        private static readonly object FakeObject = new
+        {
+            CurrentOffset = 0,
+            CurrentPage = 0,
+            Items = 0,
+            Parts = 0,
+            Previous = 0,
+            Next = 0,
+            PageSize = 0,
+            Pages = 0,
+            IsLink = 0,
+            Title = 0,
+            Url = 0
+        };
+
         public static FilterCollection WithPaginationFilters(this FilterCollection filters)
         {
             filters.AddFilter("default_pagination", DefaultPaginationFilter);
             return filters;
         }
 
-        private static readonly FluidParser Parser = new FluidParser();
+        private static string BuildPaginationTemplate(MemberNameStrategy strategy)
+        {
+            var template = DefaultPaginationTemplate;
+            var type = FakeObject.GetType();
+            foreach (var property in type.GetProperties())
+            {
+                var holder = $"__{property.Name}__";
+                var name = strategy.Invoke(property);
+                template = template.Replace(holder, name);
+            }
 
-        private const string DefaultPaginationTemplate = @"{%- if paginate.previous and paginate.previous.isLink -%}
-<span class=""prev""><a href=""{{paginate.previous.url}}"">{{ txt_previous | default: paginate.previous.title }}</a></span>
-{%-endif-%}
-{%- for pair in paginate.parts -%}
-{%- if pair.isLink -%}
-<span class=""page""><a href=""{{pair.url}}"">{{pair.title}}</a></span>
-{%- else -%}
-<span class=""deco"">{{pair.title}}</span>
-{%- endif -%}
-{%- endfor -%}
-{%- if paginate.next and paginate.next.isLink -%}
-<span class=""prev""><a href=""{{paginate.next.url}}"">{{ txt_next | default: paginate.next.title }}</a></span>
-{%- endif -%}";
+            return template;
+        }
 
         public static async ValueTask<FluidValue> DefaultPaginationFilter(
             FluidValue input,
@@ -45,8 +71,8 @@ namespace Fluid.Paginating
                 var previousText = arguments.HasNamed("previous")
                     ? arguments["previous"]
                     : NilValue.Instance;
-
-                var template = Parser.Parse(DefaultPaginationTemplate);
+                var template =
+                    Parser.Parse(BuildPaginationTemplate(context.Options.MemberAccessStrategy.MemberNameStrategy));
                 context.EnterChildScope();
                 context.SetValue("paginate", paginate);
                 context.SetValue("txt_next", nextText);
