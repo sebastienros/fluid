954d0eef84aebdc0e73271325e992cb61907c51c
Daniel Petrov <daniel221569@gmail.com>
2022-10-20 19:56:44 +0200

    Assert seconds since 1970-01-01 00:00:00 UTC

--- Fluid.Tests/MiscFiltersTests.cs
+++ Fluid.Tests/MiscFiltersTests.cs
@@ -393,15 +393,15 @@ namespace Fluid.Tests
         }
 
         [Theory]
-        [InlineData(0, "Wed Dec 31 19:00:00.000 1969 -0500")]
-        [InlineData(10, "Wed Dec 31 19:00:10.000 1969 -0500")]
-        [InlineData(-10, "Wed Dec 31 18:59:50.000 1969 -0500")]
+        [InlineData(0, "Wed Dec 31 19:00:00.000 1969 -0500 0")]
+        [InlineData(10, "Wed Dec 31 19:00:10.000 1969 -0500 10")]
+        [InlineData(-10, "Wed Dec 31 18:59:50.000 1969 -0500 -10")]
         public async Task DateNumberIsParsedAsSeconds(long number, string expected)
         {
             // Converting to Unix time should not vary by TimeZone
 
             var input = NumberValue.Create(number);
-            var format = new FilterArguments(new StringValue("%a %b %e %H:%M:%S.%L %Y %z"));
+            var format = new FilterArguments(new StringValue("%a %b %e %H:%M:%S.%L %Y %z %s"));
             var context = new TemplateContext { TimeZone = Eastern };
 
             var result = await MiscFilters.Date(input, format, context);
@@ -410,15 +410,15 @@ namespace Fluid.Tests
         }
 
         [Theory]
-        [InlineData("0", "Wed Dec 31 19:00:00.000 1969 -0500")]
-        [InlineData("1:2", "Wed Dec 31 20:02:00.000 1969 -0500")]
-        [InlineData("1:2:3.1", "Wed Dec 31 20:02:03.000 1969 -0500")]
+        [InlineData("0", "Wed Dec 31 19:00:00.000 1969 -0500 0")]
+        [InlineData("1:2", "Wed Dec 31 20:02:00.000 1969 -0500 3720")]
+        [InlineData("1:2:3.1", "Wed Dec 31 20:02:03.000 1969 -0500 3723")]
         public async Task DateTimeSpanIsParsedAsSeconds(string timespan, string expected)
         {
             // Converting to Unix time should not vary by TimeZone
 
             var input = FluidValue.Create(TimeSpan.Parse(timespan), new TemplateOptions());
-            var format = new FilterArguments(new StringValue("%a %b %e %H:%M:%S.%L %Y %z"));
+            var format = new FilterArguments(new StringValue("%a %b %e %H:%M:%S.%L %Y %z %s"));
             var context = new TemplateContext { TimeZone = Eastern };
 
             var result = await MiscFilters.Date(input, format, context);
