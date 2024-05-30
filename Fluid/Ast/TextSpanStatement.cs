4f8c5c9c721c00fcc671f60284d7864822207c17
Sébastien Ros <sebastienros@gmail.com>
2024-05-29 18:00:04 -0700

    Improve TextSpan and For statements (#668)

--- Fluid/Ast/TextSpanStatement.cs
+++ Fluid/Ast/TextSpanStatement.cs
@@ -1,16 +1,15 @@
-﻿using Parlot;
+﻿using Fluid.Utils;
+using Parlot;
 using System.Text.Encodings.Web;
-using Fluid.Utils;
 
 namespace Fluid.Ast
 {
     public sealed class TextSpanStatement : Statement
     {
-        private bool _isStripped;
-        private bool _isEmpty;
+        private bool _isBufferPrepared;
         private readonly object _synLock = new();
         private TextSpan _text;
-        private string _buffer;
+        internal string _preparedBuffer;
 
         public TextSpanStatement(in TextSpan text)
         {
@@ -32,115 +31,128 @@ namespace Fluid.Ast
 
         public ref readonly TextSpan Text => ref _text;
 
-        public override ValueTask<Completion> WriteToAsync(TextWriter writer, TextEncoder encoder, TemplateContext context)
+        public string Buffer => _preparedBuffer;
+
+        public void PrepareBuffer(TemplateOptions options)
         {
-            if (!_isStripped)
+            if (_isBufferPrepared)
             {
-                // Prevent two threads from strsipping the same statement in case WriteToAsync is called concurrently
-                // 
-                lock (_synLock)
-                {
-                    if (!_isStripped)
-                    {
-                        var trimming = context.Options.Trimming;
-                        StripLeft |=
-                            (PreviousIsTag && (trimming & TrimmingFlags.TagRight) != 0) ||
-                            (PreviousIsOutput && (trimming & TrimmingFlags.OutputRight) != 0)
-                            ;
-
-                        StripRight |=
-                            (NextIsTag && (trimming & TrimmingFlags.TagLeft) != 0) ||
-                            (NextIsOutput && (trimming & TrimmingFlags.OutputLeft) != 0)
-                            ;
-
-                        var span = _text.Buffer;
-                        var start = 0;
-                        var end = _text.Length - 1;
-
-                        // Does this text need to have its left part trimmed?
-                        if (StripLeft)
-                        {
-                            var firstNewLine = -1;
-
-                            for (var i = start; i <= end; i++)
-                            {
-                                var c = span[_text.Offset + i];
-
-                                if (Character.IsWhiteSpaceOrNewLine(c))
-                                {
-                                    start++;
-
-                                    if (firstNewLine == -1 && (c == '\n'))
-                                    {
-                                        firstNewLine = start;
-                                    }
-                                }
-                                else
-                                {
-                                    break;
-                                }
-                            }
-
-                            if (!context.Options.Greedy)
-                            {
-                                if (firstNewLine != -1)
-                                {
-                                    start = firstNewLine;
-                                }
-                            }
-                        }
-
-                        // Does this text need to have its right part trimmed?
-                        if (StripRight)
-                        {
-                            var lastNewLine = -1;
-
-                            for (var i = end; i >= start; i--)
-                            {
-                                var c = span[_text.Offset + i];
-
-                                if (Character.IsWhiteSpaceOrNewLine(c))
-                                {
-                                    if (lastNewLine == -1 && c == '\n')
-                                    {
-                                        lastNewLine = end;
-                                    }
-
-                                    end--;
-                                }
-                                else
-                                {
-                                    break;
-                                }
-                            }
-
-                            if (!context.Options.Greedy)
-                            {
-                                if (lastNewLine != -1)
-                                {
-                                    end = lastNewLine;
-                                }
-                            }
-                        }
-                        if (end - start + 1 == 0)
-                        {
-                            _isEmpty = true;
-                        }
-                        else if (start != 0 || end != _text.Length - 1)
-                        {
-                            var offset = _text.Offset;
-                            var buffer = _text.Buffer;
-
-                            _text = new TextSpan(buffer, offset + start, end - start + 1);
-                        }
-
-                        _buffer = _text.ToString();
-                        _isStripped = true;
-                    }
-                }
+                return;
             }
 
-            if (_isEmpty)
+            // Prevent two threads from stripping the same statement in case WriteToAsync is called concurrently
+            lock (_synLock)
+            {
+                if (!_isBufferPrepared)
+                {
+                    var trimming = options.Trimming;
+                    StripLeft |=
+                        (PreviousIsTag && (trimming & TrimmingFlags.TagRight) != 0) ||
+                        (PreviousIsOutput && (trimming & TrimmingFlags.OutputRight) != 0)
+                        ;
+
+                    StripRight |=
+                        (NextIsTag && (trimming & TrimmingFlags.TagLeft) != 0) ||
+                        (NextIsOutput && (trimming & TrimmingFlags.OutputLeft) != 0)
+                        ;
+
+                    var span = _text.Buffer;
+                    var start = 0;
+                    var end = _text.Length - 1;
+
+                    // Does this text need to have its left part trimmed?
+                    if (StripLeft)
+                    {
+                        var firstNewLine = -1;
+
+                        for (var i = start; i <= end; i++)
+                        {
+                            var c = span[_text.Offset + i];
+
+                            if (Character.IsWhiteSpaceOrNewLine(c))
+                            {
+                                start++;
+
+                                if (firstNewLine == -1 && (c == '\n'))
+                                {
+                                    firstNewLine = start;
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (!options.Greedy)
+                        {
+                            if (firstNewLine != -1)
+                            {
+                                start = firstNewLine;
+                            }
+                        }
+                    }
+
+                    // Does this text need to have its right part trimmed?
+                    if (StripRight)
+                    {
+                        var lastNewLine = -1;
+
+                        for (var i = end; i >= start; i--)
+                        {
+                            var c = span[_text.Offset + i];
+
+                            if (Character.IsWhiteSpaceOrNewLine(c))
+                            {
+                                if (lastNewLine == -1 && c == '\n')
+                                {
+                                    lastNewLine = end;
+                                }
+
+                                end--;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        if (!options.Greedy)
+                        {
+                            if (lastNewLine != -1)
+                            {
+                                end = lastNewLine;
+                            }
+                        }
+                    }
+                    if (end - start + 1 == 0)
+                    {
+                        _text = "";
+                    }
+                    else if (start != 0 || end != _text.Length - 1)
+                    {
+                        var offset = _text.Offset;
+                        var buffer = _text.Buffer;
+
+                        _text = new TextSpan(buffer, offset + start, end - start + 1);
+                    }
+
+                    _preparedBuffer = _text.ToString();
+                    _isBufferPrepared = true;
+                }
+            }
+        }
+
+        protected internal override Statement Accept(AstVisitor visitor) => visitor.VisitTextSpanStatement(this);
+
+        public override ValueTask<Completion> WriteToAsync(TextWriter writer, TextEncoder encoder, TemplateContext context)
+        {
+            if (!_isBufferPrepared)
+            {
+                PrepareBuffer(context.Options);
+            }
+
+            if (_preparedBuffer == "")
             {
                 return new ValueTask<Completion>(Completion.Normal);
             }
@@ -157,7 +169,7 @@ namespace Fluid.Ast
                 return Completion.Normal;
             }
 
-            var task = writer.WriteAsync(_buffer);
+            var task = writer.WriteAsync(_preparedBuffer);
             if (!task.IsCompletedSuccessfully())
             {
                 return Awaited(task);
@@ -165,7 +177,5 @@ namespace Fluid.Ast
 
             return new ValueTask<Completion>(Completion.Normal);
         }
-
-        protected internal override Statement Accept(AstVisitor visitor) => visitor.VisitTextSpanStatement(this);
     }
 }
