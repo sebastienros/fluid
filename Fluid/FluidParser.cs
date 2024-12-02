2c0d404ed7d6ed4140446c8a9b1b95c9f9b013db
Sebastien Ros <sebros@microsoft.com>
2024-12-01 16:08:15 -0800

    Remove unnecesary parser

--- Fluid/FluidParser.cs
+++ Fluid/FluidParser.cs
@@ -51,8 +51,7 @@ namespace Fluid
         protected readonly Parser<IReadOnlyList<FunctionCallArgument>> FunctionCallArgumentsList;
         protected readonly Parser<Expression> LogicalExpression;
         protected readonly Parser<Expression> CombinatoryExpression; // and | or
-        protected readonly Parser<Expression> Primary;
-        protected readonly Deferred<Expression> PrimaryInternal = Deferred<Expression>();
+        protected readonly Deferred<Expression> Primary = Deferred<Expression>();
         protected readonly Deferred<Expression> FilterExpression = Deferred<Expression>();
         protected readonly Deferred<IReadOnlyList<Statement>> KnownTagsList = Deferred<IReadOnlyList<Statement>>();
         protected readonly Deferred<IReadOnlyList<Statement>> AnyTagsList = Deferred<IReadOnlyList<Statement>>();
@@ -124,7 +123,7 @@ namespace Fluid
             Range.Name = "Range";
 
             // primary => NUMBER | STRING | property
-            PrimaryInternal.Parser =
+            Primary.Parser =
                 String.Then<Expression>(x => new LiteralExpression(StringValue.Create(x)))
                 .Or(Member.Then<Expression>(static x =>
                 {
